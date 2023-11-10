c14235e957f1aca187470d9b80bd665456d943d2
QuentinGP <quentgeoffpro@yahoo.com>
2023-11-10 11:36:00 +0100

    api authen

--- src/common/header/header.jsx
+++ src/common/header/header.jsx
@@ -1,24 +1,31 @@
-import filtre from"../../assets/pictos/sliders.svg";
-import animationHeader from './headerAnim.js';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser as farUser} from '@fortawesome/free-regular-svg-icons';
-import { faMagnifyingGlass as fasMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import filtre from "../../assets/pictos/sliders.svg";
+import animationHeader from "./headerAnim.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser as farUser } from "@fortawesome/free-regular-svg-icons";
+import { faMagnifyingGlass as fasMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  return(
+  const handleUberLogin = () => {
+    // Logique de connexion avec Uber
+    const redirectUri = encodeURIComponent('http://localhost:3000/connexion');
+    window.location.href = `https://login.uber.com/oauth/v2/authorize?client_id=dNSiA5-aHhaDC4Ue9nTrzXhJW7GhYsMR&response_type=code&redirect_uri=${redirectUri}`;
+  };
+
+  return (
     <header>
       <div className="t">
         <div>
           <p className="sub">Maintenant</p>
           <b>Lieu actuel</b>
         </div>
-        <div className="account">
-        <FontAwesomeIcon icon={farUser} />
+        <div className="account" onClick={handleUberLogin}>
+          <FontAwesomeIcon icon={farUser} />
         </div>
       </div>
       <div className="b">
         <div>
-        <FontAwesomeIcon icon={fasMagnifyingGlass} />
+          <FontAwesomeIcon icon={fasMagnifyingGlass} />
           <input
             type="text"
             name="headSearch"
@@ -28,10 +35,9 @@ const Header = () => {
         </div>
         <img src={filtre} alt="filter" />
       </div>
-      <script scr={animationHeader}></script>
+      <script src={animationHeader}></script>
     </header>
-  )
+  );
 };
-  
-  export default Header;
-  
+
+export default Header;
--- src/composants/index/cards.jsx
+++ src/composants/index/cards.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
 
 const Cards = () => {
   return (
@@ -16,33 +17,7 @@ const Cards = () => {
                           <div className="time"><p><b>30 - 50</b> min</p></div>
                           <div className="promo1">Commandez pour 25 €</div>
                           <div className="promo2">Profitez de -30%</div>
-                          <div className="heart"><FontAwesomeIcon icon={faArrowRight} /></div>
-                      </div>
-                      <div className="b">
-                          <a href=""><b>Mali food</b></a>
-                          <p className="note"><i className="fa-solid fa-star"></i>4.5 Excellent <i>(210)</i></p>
-                          <p className="km">5.5km<span>Livraison offerte</span></p>
-                      </div>
-                  </div>
-                  <div className="card">
-                      <div className="t">
-                          <div className="time"><p><b>30 - 50</b> min</p></div>
-                          <div className="promo1">Commandez pour 25 €</div>
-                          <div className="promo2">Profitez de -30%</div>
-                          <div className="heart"><FontAwesomeIcon icon={faArrowRight} /></div>
-                      </div>
-                      <div className="b">
-                          <a href=""><b>Mali food</b></a>
-                          <p className="note"><i className="fa-solid fa-star"></i>4.5 Excellent <i>(210)</i></p>
-                          <p className="km">5.5km<span>Livraison offerte</span></p>
-                      </div>
-                  </div>
-                  <div className="card">
-                      <div className="t">
-                          <div className="time"><p><b>30 - 50</b> min</p></div>
-                          <div className="promo1">Commandez pour 25 €</div>
-                          <div className="promo2">Profitez de -30%</div>
-                          <div className="heart"><FontAwesomeIcon icon={faArrowRight} /></div>
+                          <div className="heart"><FontAwesomeIcon icon={faHeart} /></div>
                       </div>
                       <div className="b">
                           <a href=""><b>Mali food</b></a>
