bb5b2447583fb630a731a95396c981bcf62781c9
QuentinGP <quentgeoffpro@yahoo.com>
2023-11-24 11:58:20 +0100

    intégration / trouver nouvelle api

--- src/common/header/header.jsx
+++ src/common/header/header.jsx
@@ -1,86 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser as farUser } from "@fortawesome/free-regular-svg-icons";
+import { faUser, faReceipt, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { faMagnifyingGlass as fasMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import filtre from "../../assets/pictos/sliders.svg"; // Ajout de l'import manquant
+import filtre from "../../assets/pictos/sliders.svg";
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import animationHeader from './headerAnim.js';
+
 const Header = () => {
-  // État local pour stocker le jeton d'accès
-  const [accessToken, setAccessToken] = useState(null);
-
-  // Effet de chargement pour vérifier si un jeton d'accès est déjà stocké dans localStorage
-  useEffect(() => {
-    const storedToken = localStorage.getItem('uberEatsAccessToken');
-    if (storedToken) {
-      setAccessToken(storedToken);
-    }
-  }, []);
-
-  const handleUberEatsLogin = () => {
-    const clientId = 'VOTRE_CLIENT_ID';
-    const redirectUri = 'VOTRE_REDIRECT_URI';
-    const authorizationUrl = `https://login.uber.com/oauth/v2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=VOS_SCOPES`;
-
-    window.location.href = authorizationUrl;
+  const toggleConnected = () => {
+    const connectedDiv = document.querySelector('.connected');
+    connectedDiv.style.display = (connectedDiv.style.display === 'none' || connectedDiv.style.display === '') ? 'block' : 'none';
   };
-
-  const handleTokenExchange = async (authorizationCode) => {
-    const clientId = 'VOTRE_CLIENT_ID';
-    const clientSecret = 'VOTRE_CLIENT_SECRET';
-    const redirectUri = 'VOTRE_REDIRECT_URI';
-
-    try {
-      const response = await axios.post('https://login.uber.com/oauth/v2/token', {
-        client_id: clientId,
-        client_secret: clientSecret,
-        grant_type: 'authorization_code',
-        redirect_uri: redirectUri,
-        code: authorizationCode,
-      });
-
-      const { access_token, expires_in, refresh_token, scope } = response.data;
-
-      localStorage.setItem('uberEatsAccessToken', access_token);
-
-      setAccessToken(access_token);
-    } catch (error) {
-      console.error('Erreur lors de l\'échange du code d\'autorisation contre le jeton d\'accès:', error);
-    }
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('uberEatsAccessToken');
-    setAccessToken(null);
-  };
-
   return (
     <header>
-      {accessToken ? (
         <div className="t">
           <div>
             <p className="sub">Maintenant</p>
             <b>Lieu actuel</b>
           </div>
-          <div className="account" onClick={handleLogout}>
+          <div className="account" onClick={toggleConnected}>
             <FontAwesomeIcon icon={farUser} />
+            <div className='connected'>
+              <ul>
+                <li>
+                  <Link><FontAwesomeIcon icon={faUser} />Mon compte</Link>
+                </li>
+                <li>
+                  <Link><FontAwesomeIcon icon={faReceipt} />Mes commandes</Link>
+                </li>
+                <li>
+                  <Link><FontAwesomeIcon icon={faRightFromBracket} />Se déconnecter</Link>
+                </li>
+              </ul>
+            </div>
           </div>
-          <p>Vous êtes connecté</p>
         </div>
-      ) : (
-        <div className="t" onClick={handleUberEatsLogin}>
-        <div>
-          <p className="sub">Maintenant</p>
-          <b>Lieu actuel</b>
-        </div>
-        <div className="account">
-          <FontAwesomeIcon icon={farUser} />
-          <div className='connexion'>
-            
-          </div>
-        </div>
-      </div>
-      )}
 
       <div className="b">
         <div>
@@ -95,6 +50,8 @@ const Header = () => {
         <img src={filtre} alt="filter" />
       </div>
       <script src={animationHeader}></script>
+      <div className="lieu">
+      </div>
     </header>
   );
 };
