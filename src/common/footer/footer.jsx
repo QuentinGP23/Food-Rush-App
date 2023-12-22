112eaa6f1684b34d23cb689e62e0f27752c25cc5
QuentinGP <quentgeoffpro@yahoo.com>
2023-12-22 14:12:19 +0100

    .

--- src/common/footer/footer.jsx
+++ src/common/footer/footer.jsx
@@ -18,12 +18,40 @@ const Footer = () => {
   const [suggestions, setSuggestions] = useState([]);
   const [error, setError] = useState("");
   const [locations, setLocations] = useState([]);
-
   useEffect(() => {
-    const savedLocations = JSON.parse(localStorage.getItem('locations')) || [];
-    setLocations(savedLocations.map(location => ({ ...location, active: false })));
+    const initLocations = async () => {
+      let savedLocations = JSON.parse(localStorage.getItem('locations')) || [];
+      let currentLocationExists = savedLocations.some(location => location.name === "Lieu actuel");
+      if (!currentLocationExists) {
+        const currentLocation = await getCurrentLocation();
+        if (currentLocation) {
+          savedLocations.unshift(currentLocation);
+        }
+      }
+      savedLocations = savedLocations.map(location => ({
+        ...location,
+        active: location.name === "Lieu actuel"
+      }));
+      setLocations(savedLocations);
+      localStorage.setItem('locations', JSON.stringify(savedLocations));
+    };
+    initLocations();
   }, []);
-
+  const getCurrentLocation = async () => {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      return {
+        name: "Lieu actuel",
+        longitude: coordinates.coords.longitude,
+        latitude: coordinates.coords.latitude,
+        active: true
+      };
+    } catch (error) {
+      console.error("Erreur lors de l'obtention de la position actuelle:", error);
+      setError("Impossible de récupérer la position actuelle");
+      return null;
+    }
+  };
   const searchAddress = async (address) => {
     const apiUrl = `https://geocode.maps.co/search?q=${encodeURIComponent(address)}`;
     try {
@@ -34,37 +62,43 @@ const Footer = () => {
       console.error("Erreur lors de la récupération des adresses:", error);
     }
   };
-
   const handleAddressInputChange = (e) => setAddressInput(e.target.value);
-
   const handleAddressSubmit = async (event) => {
     event.preventDefault();
     if (addressInput) {
       await searchAddress(addressInput);
     }
   };
-
   const handleSuggestionClick = (suggestion) => {
-    if (suggestion.display_name !== "Lieu actuel") {
-      let newLocations = JSON.parse(localStorage.getItem('locations')) || [];
-      newLocations.push({
-        name: suggestion.display_name,
-        longitude: suggestion.lon,
-        latitude: suggestion.lat,
-        active: false
-      });
-      localStorage.setItem('locations', JSON.stringify(newLocations));
-      setSuggestions([]);
-      setAddressInput('');
-      setLocations(newLocations);
-    }
+    let newLocations = [...locations].map(location => ({ ...location, active: false }));
+    newLocations.push({
+      name: suggestion.display_name,
+      longitude: suggestion.lon,
+      latitude: suggestion.lat,
+      active: true
+    });
+    localStorage.setItem('locations', JSON.stringify(newLocations));
+    setSuggestions([]);
+    setAddressInput('');
+    setLocations(newLocations);
   };
-  
   const handleDeleteLocation = (index) => {
     let updatedLocations = [...locations];
+    const wasActive = updatedLocations[index].active;
     updatedLocations.splice(index, 1);
-    localStorage.setItem('locations', JSON.stringify(updatedLocations));
+    if (wasActive) {
+      const currentLocationIndex = updatedLocations.findIndex(
+        location => location.name === "Lieu actuel"
+      );
+      if (currentLocationIndex > -1) {
+        updatedLocations = updatedLocations.map((location, idx) => ({
+          ...location,
+          active: idx === currentLocationIndex
+        }));
+      }
+    }
     setLocations(updatedLocations);
+    localStorage.setItem('locations', JSON.stringify(updatedLocations));
   };
   const handleLocationSelect = (index) => {
     const updatedLocations = locations.map((location, idx) => ({
@@ -74,48 +108,22 @@ const Footer = () => {
     localStorage.setItem('locations', JSON.stringify(updatedLocations));
     setLocations(updatedLocations);
   };
-  const getCurrentLocation = async () => {
-    try {
-      const coordinates = await Geolocation.getCurrentPosition();
-      let updatedLocations = JSON.parse(localStorage.getItem('locations')) || [];
-      const existingCurrentLocationIndex = updatedLocations.findIndex(
-        location => location.name === "Lieu actuel"
-      );
-      
-      if (existingCurrentLocationIndex > -1) {
-        updatedLocations[existingCurrentLocationIndex] = {
-          ...updatedLocations[existingCurrentLocationIndex],
-          longitude: coordinates.coords.longitude,
-          latitude: coordinates.coords.latitude,
-          active: true
-        };
-      } else {
-        updatedLocations.push({
-          name: "Lieu actuel",
-          longitude: coordinates.coords.longitude,
-          latitude: coordinates.coords.latitude,
-          active: true
-        });
-      }
-  
-      updatedLocations = updatedLocations.map((location, index) => ({
-        ...location,
-        active: index === existingCurrentLocationIndex
-      }));
-    
-      localStorage.setItem('locations', JSON.stringify(updatedLocations));
-      setLocations(updatedLocations);
-    } catch (error) {
-      console.error("Erreur lors de l'obtention de la position actuelle:", error);
-    }
-  };
 
   const handleCurrentLocationChange = async (event) => {
     if (event.target.checked) {
-      await getCurrentLocation();
+      const currentLocation = await getCurrentLocation();
+      if (currentLocation) {
+        let updatedLocations = [...locations];
+        updatedLocations = updatedLocations.map(location => ({
+          ...location,
+          active: false
+        }));
+        updatedLocations.unshift(currentLocation);
+        setLocations(updatedLocations);
+        localStorage.setItem('locations', JSON.stringify(updatedLocations));
+      }
     }
   };
-
   return (
     <footer>
       <div className="lieu">
@@ -142,30 +150,23 @@ const Footer = () => {
               </div>
             ))}
         </div>
-        <div>
-          <input type="radio" name="Lieuactuel" id="Lieuactuel" onChange={handleCurrentLocationChange} checked/>
-          <label htmlFor="Lieuactuel">
-            <div>
-              <FontAwesomeIcon icon={faLocationDot} />
-              <p>Lieu actuel</p>
-            </div>
-          </label>
-        </div>
         {locations.map((location, index) => (
           <div key={index}>
             <input
-            type="radio"
-            name="Lieuactuel"
-            id={`location-${index}`}
-            checked={location.actif}
-            onChange={() => handleLocationSelect(index)}
-          />
+              type="radio"
+              name="Lieuactuel"
+              id={`location-${index}`}
+              checked={location.active}
+              onChange={() => handleLocationSelect(index)}
+            />
             <label htmlFor={`location-${index}`}>
               <div>
                 <FontAwesomeIcon icon={faLocationDot} />
                 <p>{location.name}</p>
               </div>
-              <p className="modif" onClick={() => handleDeleteLocation(index)}>Supprimer</p>
+              {location.name !== "Lieu actuel" && (
+                <p className="modif" onClick={() => handleDeleteLocation(index)}>Supprimer</p>
+              )}
             </label>
           </div>
         ))}
